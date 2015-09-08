9766d052160dfb8b65dd2105845bc7581ff5f45b
daariff <aariff.deen@gmail.com>
2015-09-08 12:24:53 -0700

    Addeed angular functionality

---
+++ public/javascript/angular/controllers.js
@@ -0,0 +1,13 @@
+var Controllers = angular.module('Controllers')
+
+Controllers.controller('iconCtrl', function($scope) {
+
+    $scope.images = [
+        {'marketingsolutions'},
+        {'productbranding'},
+        {'onlineadvertising'},
+        {'digitalstrategy'},
+    ];
+    
+});
+
