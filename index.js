5f63fae87c8465bb1fe36f383fcba0fb53a97a40
daariff <aariff.deen@gmail.com>
2015-09-16 15:17:09 -0700

    fixing port issue

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ app.get('/', function(req, res){
 //app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
 app.use(express.static(__dirname + '/public'));
 
-http.listen(3000, function(){
-      console.log('listening on: 3000');
+app.listen(process.env.PORT || 3000, function() {
+      console.log("listening on 3000");
 });
+//http.listen(3000, function(){
+//      console.log('listening on: 3000');
+//});
