afec0628764019fc01868fd222db03eb79c0e2f8
daariff <aariff.deen@gmail.com>
2015-09-16 13:16:54 -0700

    added icons w/ angular

--- public/javascript/angular/controllers.js
+++ public/javascript/angular/controllers.js
@@ -3,10 +3,33 @@ var Controllers = angular.module('Controllers',[])
 Controllers.controller('iconCtrl', function($scope) {
 
     $scope.images = [
-        {name: 'marketingsolutions'},
-        {name: 'productbranding'},
-        {name: 'onlineadvertising'},
-        {name: 'digitalstrategy'},
+        {name: '3D Animation',
+		 img: '3d animation.png'
+		},
+        {name: 'Product Branding',
+         img: 'branding.png'
+    	},
+        {name: 'Creative Design',
+    	 img: 'creative design.png'
+    	},
+        {name: 'Digital Campaign Solutions',
+    	 img: 'digital campaign solution.png'
+    	},
+    	{name: 'Live Events',
+    	 img: 'events.png'
+    	},
+    	{name: 'Marketing Campaign Solutions',
+    	 img: 'marketing campaign solution.png'
+    	},
+    	{name: 'New Media Advertising',
+    	 img: 'new media advertising.png'
+    	},
+    	{name: 'Projection Mapping',
+    	 img: 'projection mapping.png'
+    	},
+    	{name: 'Stakeholder Managment',
+    	 img: 'stakeholder management.png'
+    	},
     ];
 
 });
